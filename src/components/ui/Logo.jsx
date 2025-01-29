0933b1f9ba351677c2994af9727c4e92cf952322
whoishasan <uniquest.hasan@gmail.com>
2025-01-29 18:38:28 +0600

    logo done

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import LogUri from "@/assets/logo.webp";
 
-const Logo = () => {
-  return <div></div>;
+const Logo = ({ className, textClass, ...props }) => {
+  return (
+    <>
+      <Link to={"/"} className={`flex items-center gap-5 ${className}`}>
+        <img width={50} src={LogUri} {...props} alt="Logo" />
+        <p className={`text-2xl font-bold hidden sm:block ${textClass}`}>
+          Donor. Flow
+        </p>
+      </Link>
+    </>
+  );
 };
 
 export default Logo;
