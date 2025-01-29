a09b367ea845f1c6a4ba048c66457611e2ef628c
whoishasan <uniquest.hasan@gmail.com>
2025-01-29 18:25:50 +0600

    title and des done

--- src/components/TitleAndDes.jsx
+++ src/components/TitleAndDes.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-
-const TitleAndDes = () => {
-  return <div></div>;
+const TitleAndDes = ({ title, children }) => {
+  return (
+    <>
+      <section data-aos="fade-up" className="flex justify-center pt-40">
+        <div className="w-primary px-5 justify-center text-center flex flex-col items-center">
+          <h3 className="text-4xl font-semibold">{title}</h3>
+          <span className="w-20 h-1 bg-red-500 flex my-3"></span>
+          <p>{children}</p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default TitleAndDes;
