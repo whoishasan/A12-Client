0ce7312e94642d358e1dc0740a5d01f82a0aa9a8
whoishasan <uniquest.hasan@gmail.com>
2025-01-29 18:32:49 +0600

    button done

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,7 +1,11 @@
-import React from "react";
-
-const button = () => {
-  return <div></div>;
-};
-
-export default button;
+export default function button({ children, className }) {
+  return (
+    <>
+      <button
+        className={`bg-red-500 border-dashed border-2 hover:rounded-2xl hover:bg-red-900 transition-all rounded-md text-white font-bold py-2 px-4 rounded ${className}`}
+      >
+        {children}
+      </button>
+    </>
+  );
+}
