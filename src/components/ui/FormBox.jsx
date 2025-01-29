8ff61707948c6c46ef8984401827881826dc0e33
whoishasan <uniquest.hasan@gmail.com>
2025-01-29 18:34:26 +0600

    formbox section done

--- src/components/ui/FormBox.jsx
+++ src/components/ui/FormBox.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { motion } from "motion/react";
 
-const FormBox = () => {
-  return <div></div>;
+const FormBox = ({ className, children }) => {
+  return (
+    <>
+      <motion.div
+        initial={{ translateY: 200, opacity: 0 }}
+        animate={{ translateY: 0, opacity: 1 }}
+        transition={{ duration: 0.6 }}
+        className={`rounded-2xl border p-5 sm:p-10 bg-white ${className}`}
+      >
+        {children}
+      </motion.div>
+    </>
+  );
 };
 
 export default FormBox;
