48c9c3d567582b319f03f7c3189151607d66a111
whoishasan <uniquest.hasan@gmail.com>
2025-01-29 18:47:28 +0600

    line error done

--- src/components/ui/LineError.jsx
+++ src/components/ui/LineError.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { AnimatePresence, motion } from "framer-motion"; // Ensure the correct import from framer-motion
+import { MdOutlineErrorOutline } from "react-icons/md";
 
-const LineError = () => {
-  return <div></div>;
+const LineError = ({ className, error }) => {
+  return (
+    <AnimatePresence>
+      {error && ( // Check if error exists to render the error message
+        <motion.span
+          className={`text-red-500 flex gap-2 text-[13.7px] items-center ${className} mt-3 -mb-3`}
+          initial={{ opacity: 0, height: 0 }}
+          animate={{ opacity: 1, height: "auto" }}
+          exit={{ opacity: 0, height: 0 }}
+        >
+          <MdOutlineErrorOutline />
+          {error}
+        </motion.span>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default LineError;
